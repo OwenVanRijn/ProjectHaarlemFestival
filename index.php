7f70eb6c565750aa7f3dbd5cd28d8510e446e1b2
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-02-11 11:38:18 +0100

    Homepage update

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>STARTPAGE</title>
+    <link rel="stylesheet" href="Style/style.css">
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Haarlem, festival, jazz, food, history, party, feest, geschiedenis, eten, restaurant">
+    <meta name="description" content="Haarlem Festival">
+    <meta name="author" content="Haarlem Festival">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+ 
+<h1 class="main-title">Welcome to Haarlem Festival</h1>
+<p class="main-title under-title">Haarlem Festival, for the people that enjoy
+<br>a good time</p>
+
+
+    <h1>Welcome to Haarlem Festival</h1>
+    <p>Haarlem is a busy city, with a rich history and beatiful statues and other sights. Walk trough cozy streets in the heart of Haarlem and discover a large variety of stores. Sit down at one of the many pubs with your friends for a drink, or visit one
+        of the many museums or theaters in Haarlem. Haarlem even has a beach you can unwind at. In any case, Haarlem bursts with possibilities for something to do on a weekend out. To show you Haarlem, we've organised a few events in it! It's called "The
+        Haarlem Festival". If you love <a href="dance.php">dance</a> or <a href="jazz.php">jazz</a> we have a few events planned to help you explore Haarlem. We also have a selection of <a href="restaurants.php">restaurants</a> for you to have dinner
+        at.
+    </p>
+
+
+    <h1>Discover Haarlem Festival</h1>
+
+
+    <section class="row">
+        <section class="column">
+            <h2><a href="dance.php">Dance</a></h2>
+            <img class="eventimg" src="Pictures/Dance.png">
+            <p>Our dance acts focus on dance, house, techno and trance.
+                <br>
+                Haarlem Festival offers 6 of the best DJs in the world. They will perform in back2back and club sessions.
+            </p>
+        </section>
+
+        <section class="column">
+            <h2><a href="jazz.php">Jazz</a></h2>
+            <img class="eventimg" src="Pictures/Jazz.png">
+            <p>Read moreJazzJazz is one of the main acts at the Haarlem Festival.
+                <br>
+                We have artists such as Jonna Frazer, Soul Six and Evolve in house.
+                <br>
+                We also offer a number of free performances.
+            </p>
+        </section>
+
+        <section class="column">
+            <h2><a href="food.php">Food</a></h2>
+            <img class="eventimg" src="Pictures/Food.png">
+            <p>We have selected several restaurants in the center of Haarlem for you. These restaurants have delicious dishes for you. Both national and international dishes. The restaurants have a delicious menu for you at a discounted price.
+            </p>
+        </section>
+    </section>
+
+
+    <h1>This is Haarlem Festival</h1>
+
+
+    <p>Together we create an unforgettable musical memory. Our own stories with national and international artists.</p>
+
+
+    <p>Enjoy the scents and taste of the national and international dishes of the restaurants nearby.</p>
+
+    <p>Discover, meet, experience. Find out what the music does to you. Meet and share your experiences.</p>
+
+</body>
+
+</html>
