381e92ac68d12b6b411dcccbac24e5f0c9eaf238
Ryan-Erfmann <57288223+Ryan-Erfmann@users.noreply.github.com>
2021-02-15 12:44:44 +0100

    jazz.php

---
+++ jazz.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Haarlem, festival, jazz, food, history, party, feest, geschiedenis, eten, restaurant">
+    <meta name="description" content="Haarlem Festival">
+    <meta name="author" content="Haarlem Festival">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Jazz</title>
+  </head>
+  <body>
+    <header class="title">
+
+      <h1 class="main-title">Jazz</h1>
+      <p class="main-title under-title">Haarlem Festival, for the people that enjoy a good time</p>
+
+    </header>
+
+    <main class="content">
+      <h1>Our Haarlem Jazz performances.</h1>
+      <section>
+
+        <section id="filter">
+          <section class="filterdays">
+            <label for="days">Day</label>
+            <select name="days">
+              <option value="1">Thursday 26th July</option>
+              <option value="2">Friday 27th July</option>
+              <option value="3">Saturday 28th July</option>
+              <option value="4">Sunday 29th July</option>
+            </select>
+
+          </section>
+
+          <section class="filterlocation">
+            <label for="locations">Location</label>
+            <select name="locations">
+              <option value="1">Main Hall</option>
+              <option value="2">Second Hall</option>
+              <option value="3">Third Hall</option>
+              <option value="4">Grote Markt</option>
+            </select>
+
+          </section>
+
+          <section class="searchbar">
+            <label for="SearchBar">Search</label>
+            <input type="text" name="SearchBar" placeholder="Search..">
+             <button type="submit"><i class="fa fa-search"></i></button>
+          </section>
+
+        </section>
+      </section>
+    </main>
+  </body>
+</html>
