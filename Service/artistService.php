44e28cfaea4899e4e024343b7910f1dd1f5605da
Lou <louellacreemers@gmail.com>
2021-02-21 18:35:25 +0100

    Update artistService.php

--- Service/artistService.php
+++ Service/artistService.php
@@ -2,15 +2,15 @@
 
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 
-require_once ("Model/danceArtist.php");
-require_once ("DAL/dynamicQueryGen.php");
+require_once ($root . "/Model/danceArtist.php");
+require_once ($root . "/DAL/danceArtistDAO.php");
 require_once ("baseService.php");
 
 class artistService extends baseService
 {
     public function __construct()
     {
-        parent::__construct(danceArtist::class);
+        $this->db = new danceArtistDAO();
     }
 
     public function getArtists(){
