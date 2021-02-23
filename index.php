78beb8b1f5ec23a6e157b67ff354ab4e8ce909a2
Lou <louellacreemers@gmail.com>
2021-02-23 14:29:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 <html>
 
 <head>
-    <title>STARTPAGE</title>
+    <title>Home - Haarlem Festival</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="keywords" content="Haarlem, festival, jazz, food, history, party, feest, geschiedenis, eten, restaurant">
@@ -27,82 +28,92 @@
         </div>
     </header>
 
-    <main class="content">
+    <section class="container-fluid h-100">
         <h1>Welcome to Haarlem Festival</h1>
-        <p>Haarlem is a busy city, with a rich history and beatiful statues and other sights. Walk trough cozy streets in the heart of Haarlem and discover a large variety of stores. Sit down at one of the many pubs with your friends for a drink, or visit one
-            of the many museums or theaters in Haarlem. Haarlem even has a beach you can unwind at. In any case, Haarlem bursts with possibilities for something to do on a weekend out. To show you Haarlem, we've organised a few events in it! It's called "The
-            Haarlem Festival". If you love <a href="dance.php">dance</a> or <a href="jazz.php">jazz</a> we have a few events planned to help you explore Haarlem. We also have a selection of <a href="restaurants.php">restaurants</a> for you to have dinner
-            at.
-        </p>
 
+        <section class="row justify-content-center align-self-center text-center">
+            <section class ="col-10">
+                <p>Haarlem is a busy city, with a rich history and beautiful statues and other sights. 
+                Walk trough cozy streets in the heart of Haarlem and discover a large variety of stores. Sit down at one of the 
+                many pubs with your friends for a drink, or visit one of the many museums or theaters in Haarlem. Haarlem even has a beach 
+                you can unwind at. In any case, Haarlem bursts with possibilities for something to do on a weekend out.
+                </p>
+            </section>
+        </section>
 
-
-
+        <section class="row justify-content-center align-self-center text-center">
+            <section class="col-10">
+                <p> To show you Haarlem, we've organised a few events in it! It's called "The Haarlem Festival". If you love <a href="dance.php">dance</a> or 
+                <a href="jazz.php">jazz</a> we have a few events planned to help you explore Haarlem. 
+                We also have a selection of <a href="food.php">restaurants</a> for you to have dinner at.
+                </p>
+            </section>
+        </section>
 
 
         <h1>Discover Haarlem Festival</h1>
 
 
-        <section class="row">
-            <section class="discoverBlockColumn">
-                <h2><a href="dance.php">Dance</a></h2>
-                <img class="eventimg" src="img/Dance.png">
-                <p class="discoverBlockContent">Our dance acts focus on dance, house, techno and trance.
-                    <br>
-                    Haarlem Festival offers 6 of the best DJs in the world. They will perform in back2back and club sessions.
-                </p>
-                <a href="dance.php" class="button1">Read more</a>
+        <section class="row justify-content-center align-self-center text-center">
+            <section class="col-3">
+                <section class="col-12" style="background-color: black; margin-top: 2%;">
+                    <h2><a href="dance.php">Dance</a></h2>
+                    <img class="eventimg" src="img/Dance.png">
+                    <p style="margin-bottom: 0px; padding: 10%" class="discoverBlockContent">Our dance acts focus on dance, house, techno and trance.
+                        Haarlem Festival offers 6 of the best DJs in the world. They will perform in back2back and club sessions.
+                    </p>
+                </section>
+                <a href="dance.php" class="btn button1 w-100">Read more</a>
             </section>
 
-            <section class="discoverBlockColumn">
-                <h2><a href="jazz.php">Jazz</a></h2>
-                <img class="eventimg" src="img/Jazz.png">
-                <p class="discoverBlockContent">Jazz is one of the main acts at the Haarlem Festival.
+            <section class="col-3 h-100">
+                <section class="col-12" style="background-color: black; margin-top: 2%;">
+                    <h2><a href="jazz.php">Jazz</a></h2>
+                    <img class="eventimg" src="img/Jazz.png">
+                    <p style="margin-bottom: 0px; padding: 6.5%;" class="discoverBlockContent">
+                    Jazz is one of the main acts at the Haarlem Festival.
                     <br>
                     We have artists such as Jonna Frazer, Soul Six and Evolve in house.
                     <br>
                     We also offer a number of free performances.
-                </p>
-                <a href="jazz.php" class="button1">Read more</a>
+                    </p>
+                </section>
+                <a href="jazz.php" class="btn button1 w-100">Read more</a>
             </section>
 
-            <section class="discoverBlockColumn">
-                <h2><a href="food.php">Food</a></h2>
-                <img class="eventimg" src="img/Food.png">
-                <p class="discoverBlockContent">We have selected several restaurants in the center of Haarlem for you. These restaurants have delicious dishes for you. Both national and international dishes. The restaurants have a delicious menu for you at a discounted price.
-                </p>
-                <a href="food.php" class="button1">Read more</a>
-            </section>
-        </section>
-
-
-        <h1 id="WelcomToHF">This is Haarlem Festival</h1>
-
-
-
-        <section class="row">
-            <section class="thisisBlockColumn">
-                <svg class="thisisimg" viewBox="63.912 0 46.839 67.618">
-                    <path id="musical-note" d="M 106.9571762084961 43.00255966186523 C 109.058723449707 39.48612213134766 110.4528121948242 35.03855133056641 110.7233047485352 30.63780403137207 C 110.8949737548828 27.81320762634277 110.2655410766602 24.84816360473633 108.9026641845703 22.05998229980469 C 106.7647018432617 17.68003845214844 103.0922164916992 15.21957302093506 99.54456329345703 12.84233474731445 C 96.89683532714844 11.06851100921631 94.39476013183594 9.393521308898926 92.62612915039063 7.000678539276123 L 92.30881500244141 6.574127674102783 C 91.26325225830078 5.169633865356445 90.08243560791016 3.577872753143311 89.89516448974609 2.241001844406128 C 89.70790100097656 0.8885256052017212 88.48027038574219 -0.07381338626146317 87.13819885253906 0.004214017651975155 C 85.77532196044922 0.09784699976444244 84.71934509277344 1.226644515991211 84.71934509277344 2.594726324081421 L 84.71934509277344 48.94305419921875 C 82.53977203369141 47.62179183959961 79.85563659667969 46.810302734375 76.91659545898438 46.810302734375 C 69.74847412109375 46.810302734375 63.91201782226563 51.47634887695313 63.91201782226563 57.21397018432617 C 63.91201782226563 62.95159149169922 69.74847412109375 67.61762237548828 76.91659545898438 67.61762237548828 C 84.08472442626953 67.61762237548828 89.92118072509766 62.95159149169922 89.92118072509766 57.21397018432617 L 89.92118072509766 26.89768600463867 C 93.8277587890625 28.39061164855957 100.2208099365234 32.19315338134766 101.9790344238281 41.07267761230469 C 101.6513214111328 41.55644607543945 101.3391952514648 42.07662963867188 100.9750747680664 42.49277496337891 C 100.0231399536133 43.56955337524414 100.1271820068359 45.21333312988281 101.2091598510742 46.16007232666016 C 102.2807312011719 47.11720657348633 103.9245147705078 47.00276184082031 104.8764495849609 45.92598724365234 C 105.573486328125 45.13530349731445 106.1925048828125 44.20938110351563 106.7855224609375 43.24184036254883 C 106.8479385375977 43.16901397705078 106.9053268432617 43.09099197387695 106.9571762084961 43.00255966186523 Z">
-                    </path>
-                </svg>
-                <p>Together we create an unforgettable musical memory. Our own stories with national and international artists.</p>
-            </section>
-
-            <section class="thisisBlockColumn">
-                <img class="thisisimg" src="img/Icons/thisis_food.png">
-                <p>Enjoy the scents and taste of the national and international dishes of the restaurants nearby.</p>
-            </section>
-
-            <section class="thisisBlockColumn">
-                <svg class="thisisimg" viewBox="0 30 74.382 67.62">
-                    <path id="Path_12" d="M 54.6242790222168 29.99999618530273 C 50.58803939819336 29.99999618530273 46.88753509521484 31.31709289550781 43.62576675415039 33.91477584838867 C 40.49867630004883 36.40519714355469 38.41670608520508 39.57720565795898 37.19099807739258 41.88376617431641 C 35.96529388427734 39.57705688476563 33.88332366943359 36.40519714355469 30.75622940063477 33.91477584838867 C 27.49446296691895 31.31709289550781 23.79396057128906 29.99999618530273 19.75771903991699 29.99999618530273 C 8.494075775146484 29.99999618530273 0 39.4872932434082 0 52.06835556030273 C 0 65.6602783203125 10.59696578979492 74.95966339111328 26.63936042785645 89.03764343261719 C 29.36360168457031 91.42842102050781 32.45146942138672 94.13831329345703 35.66093826293945 97.02848052978516 C 36.08398818969727 97.40995025634766 36.62732315063477 97.6199951171875 37.19099807739258 97.6199951171875 C 37.75467681884766 97.6199951171875 38.29801177978516 97.40995025634766 38.72106170654297 97.02861785888672 C 41.93081665039063 94.13801574707031 45.0185432434082 91.42827606201172 47.744384765625 89.03614807128906 C 63.7850341796875 74.95966339111328 74.38199615478516 65.6602783203125 74.38199615478516 52.06835556030273 C 74.38199615478516 39.4872932434082 65.88792419433594 29.99999618530273 54.6242790222168 29.99999618530273 Z">
-                    </path>
-                </svg>
-                <p>Discover, meet, experience. Find out what the music does to you. Meet and share your experiences.</p>
+            <section class="col-3 h-100">
+                <section class="col-12" style="background-color: black; margin-top: 2%;">
+                    <h2><a href="food.php">Food</a></h2>
+                    <img class="eventimg" src="img/Food.png">
+                    <p style="margin-bottom: 0px; padding: 6.5%;" class="discoverBlockContent">
+                        We have selected several restaurants in the center of Haarlem for you. 
+                    These restaurants have delicious dishes for you. Both national and international dishes. 
+                    The restaurants have a delicious menu for you at a discounted price.
+                    </p>
+                </section>
+                <a href="food.php" class="btn button1 w-100">Read more</a>
             </section>
         </section>
-    </main>
+
+        <h1 style="margin-top: 2%">This is Haarlem Festival</h1>
+
+        <section class="row justify-content-center align-self-center text-center">
+            <section class="col-3">
+            <section class="row justify-content-center align-self-center text-center"><img src="img/Icons/thisis_musicnote.png" width="32" height="32"> </section>
+                <section class="row" style="padding: 2%;"><p>Together we create an unforgettable musical memory. Our own stories with national and international artists.</p></section>
+            </section>
+
+            <section class="col-3">
+            <section class="row justify-content-center align-self-center text-center"><img src="img/Icons/thisis_food.png" width="32" height="32"> </section>
+                <section class="row" style="padding: 2%;"><p>Enjoy the scents and taste of the national and international dishes of the restaurants nearby.</p></section>
+            </section>
+
+            <section class="col-3">
+                <section class="row justify-content-center align-self-center text-center"><img src="img/Icons/like.png"> </section>
+                <section class="row" style="padding: 2%;"><p>Discover, meet, experience. Find out what the music does to you. Meet and share your experiences.</p></section>
+            </section>
+        </section>
+    </section>
 </body>
 
 </html>
