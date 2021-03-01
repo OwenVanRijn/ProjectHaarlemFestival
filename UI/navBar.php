cd9146cbeae39a3e677c0230b53b1f5c9445025c
Ryan-Erfmann <57288223+Ryan-Erfmann@users.noreply.github.com>
2021-03-01 10:09:59 +0100

    nav bar + footer

--- UI/navBar.php
+++ UI/navBar.php
@@ -8,35 +8,43 @@ $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 <html>
 
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/navBarFooter.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <a class="navbar-brand" href="./index.php" style="margin-left: 2%">
-            <img src="./img/logo-wit.png" width="30" height="30">
-        </a>
-
-        <ul class="navbar-nav">
-            <li class = "nav-item">
-                <a class="nav-link" href="./index.php">Home</a>
-            </li>
-            <li class = "nav-item">
-                <a class="nav-link" href="./jazz.php">Jazz</a>
-            </li>
-            <li class = "nav-item">
-                <a class="nav-link" href="./dance.php">Dance</a>
-            </li>
-            <li class = "nav-item">
-                <a class="nav-link" href="./food.php">Food</a>
-            </li>
-            <li class = "nav-item">
-                <a class="nav-link" href="./contact.php">Contact us</a>
-            </li>
-            <li class = "nav-item">
-                <a class="nav-link" href="./shoppingcart.php">Shoppingcart</a>
-            </li>
+    <nav>
+        <ul>
+          <li class="navItem"><a class="image"><img class="logo" src="img/logo-wit.png" alt="logo"></a></li>
+          <li><a class="navItem" href="./index.php">Home</a></li>
+          <li><a class="navItem" class="navItem" href="./jazz.php">Jazz</a></li>
+          <li><a class="navItem" href="./food.php">Food</a></li>
+          <li><a class="navItem" href="./dance.php">Dance</a></li>
+          <li><a class="navItem" href="./contact.php">Contact</a></li>
+          <li><a class="imageRight"><img class="logo" src="img/account.png" alt="logo"></a></li>
+          <li><a class="imageRight"><img class="logo" src="img/shoppingcart.png" alt="logo"></a></li>
         </ul>
-    </nav>
+     </nav>
+     <footer>
+       <img class="logoFooter" src="img/logo-wit.png" alt="">
+      <section class="footerLinks">
+        <ul>
+          <li><a href="#">> Photo's and video's</a>
+          </li>
+          <li><a href="#">> Online events</a>
+          </li>
+          <li><a href="./contact.php">> Contact us</a>
+          </li>
+          <li><a href="./volunteers.php">> For volunteers</a>
+          </li>
+        </ul>
+      </section>
+      <section class="socialMedia">
+        <p class="titleSocialMedia">Social Media</p>
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-youtube"></a>
+      </section>
+    </footer>
+
 </body>
 </html>
