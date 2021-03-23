5fb44b59e7008bc3873275c5637f7dce8dc2c210
Lou <louellacreemers@gmail.com>
2021-03-23 16:03:12 +0100

    Update restaurantTypeLinkService.php

--- Service/restaurantTypeLinkService.php
+++ Service/restaurantTypeLinkService.php
@@ -6,7 +6,7 @@ require_once($root . "/Model/restaurantTypeLink.php");
 require_once($root . "/Model/restaurant.php");
 require_once($root . "/Service/baseService.php");
 require_once($root . "/DAL/dbContains.php");
-require_once($root . "/DAL/restaurantTypeLinkDAO.php");
+require_once("./DAL/restaurantTypeLinkDAO.php");
 require_once($root . "/DAL/restaurantTypeDAO.php");
 
 class restaurantTypeLinkService extends baseService
