45ea36c817c3d8117e4633a901f4f6d682c95f7d
Lou <louellacreemers@gmail.com>
2021-03-23 16:09:36 +0100

    Update restaurant.php

--- restaurant.php
+++ restaurant.php
@@ -49,188 +49,189 @@ if (isset($_POST["gotooverview"])) {
 <main class="content">
     <section>
         <?php
-        if (isset($_GET["restaurantId"])) {
-            try {
-                $restaurantId = $_GET["restaurantId"];
+//        if (isset($_GET["restaurantId"])) {
+//            try {
+//                $restaurantId = $_GET["restaurantId"];
+//
+//                if (!intval($restaurantId)) {
+//                    throw new Exception("Invalid id in addressbar.");
+//                }
+//                $restaurant = $restaurantService->getById($restaurantId);
 
-                if (!intval($restaurantId)) {
-                    throw new Exception("Invalid id in addressbar.");
-                }
-                $restaurant = $restaurantService->getById($restaurantId);
+//                if ($restaurant != null) {
+//                    echoRestaurant($restaurant);
+//                    ?>
+<!--                    <form method="post" action="foodreservation.php">-->
+<!--                        <input name="restaurantId" type="hidden" value="--><?php //echo $restaurantId ?><!--">-->
+<!--                        <input type="submit" class='btn button1' id="buttonReservation" name="makereservation"-->
+<!--                               value="Make a reservation"></input>-->
+<!--                    </form>-->
+<!--                    <form method="post" action=food.php>-->
+<!--                        <input type="submit" class='btn button1' id="buttonOverview" name="gotooverview"-->
+<!--                               value="Go back to overview"></input>-->
+<!--                    </form>-->
+<!--                    --><?php
+//                } else {
+//                    throw new Exception("Could not get information about this restaurant.");
+//                }
+//            } catch (Exception $exception) {
+//                $excMessage = $exception->getMessage();
+//                ?>
+<!--                <h1 class="header1Left">Restaurant not found</h1>-->
+<!--                <p>--><?php //echo $excMessage ?><!--</p>-->
+<!--                --><?php
+//            }
+//        } else {
+//            header("Location: food.php");
+//        }
 
-                if ($restaurant != null) {
-                    echoRestaurant($restaurant);
-                    ?>
-                    <form method="post" action="foodreservation.php">
-                        <input name="restaurantId" type="hidden" value="<?php echo $restaurantId ?>">
-                        <input type="submit" class='btn button1' id="buttonReservation" name="makereservation"
-                               value="Make a reservation"></input>
-                    </form>
-                    <form method="post" action=food.php>
-                        <input type="submit" class='btn button1' id="buttonOverview" name="gotooverview"
-                               value="Go back to overview"></input>
-                    </form>
-                    <?php
-                } else {
-                    throw new Exception("Could not get information about this restaurant.");
-                }
-            } catch (Exception $exception) {
-                $excMessage = $exception->getMessage();
-                ?>
-                <h1 class="header1Left">Restaurant not found</h1>
-                <p><?php echo $excMessage ?></p>
-                <?php
-            }
-        } else {
-            header("Location: food.php");
-        }
-        function echoRestaurant($restaurant)
-        {
-            $restaurantName = $restaurant->getName();
-            $description = $restaurant->getDescription();
-            $stars = $restaurant->getStars();
-            $restaurantId = $restaurant->getId();
-            $location = $restaurant->getLocation()->getAddress() . ", " . $restaurant->getLocation()->getPostalCode();
-            $parking = $restaurant->getParking();
-            $price = $restaurant->getPrice();
-            $childrenPrice = $restaurant->getPrice() / 2;
-
-            $seats = $restaurant->getSeats();
-
-            $restaurantService = new restaurantService();
-            $times = $restaurantService->getTimesByRestaurantId($restaurantId);
-            $dates = $restaurantService->getDatesByRestaurantId($restaurantId);
-
-            // Contact
-            $phoneNumber = $restaurant->getPhoneNumber();
-            $website = $restaurant->getWebsite();
-            $contactpage = $restaurant->getContact();
-            $menu = $restaurant->getMenu();
-            ?>
-
-            <section class="container">
-                <section class="restaurantContentDescription">
-                    <h2 id='starsHeader'><?php echo $restaurantName ?></h2>
-                    <?php
-                    for ($x = 0; $x < $stars; $x++) {
-                        echo "<img class='stars' src='/img/icons/star.png' alt='ster'>";
-                    }
-
-                    if (!empty($description)) {
-                        ?>
-
-                        <p> <?php echo $description ?></p>
-
-                        <?php
-                    }
-
-                    if (!empty($parking)) {
-                        ?>
-                        <br><h3>Parking information</h3>
-                        <p><?php echo $parking ?></p>
-                        <?php
-                    }
-                    ?>
-                    <br>
-                    <p><img src="/img/Restaurants/restaurant<?php echo $restaurantId ?>.png"
-                            alt="Photo of <?php echo $restaurantName ?>" width="300"></p>
-                </section>
-                <section class="restaurantContentInfoPictureCosts">
-                    <img class="imgIcons" src="/img/icons/costs.png" alt="costs">
-                </section>
-                <section class="restaurantContentInfoCosts">
-                    <h3 class="informationRestaurantLabel">Costs</h3>
-                    <?php
-                    if (!empty($price) && !empty($childrenPrice)) {
-                        ?>
-                        <p><i>Diner costs</i></p>
-                        <p>€<?php echo $price ?> p.p.</p>
-                        <sup>Children til 12 years: € <?php echo $childrenPrice ?> p.p.</sup>
-
-                        <p><i>Reservation costs</i></p>
-                        <p>€10 </p>
-                        <?php
-                    } else {
-                        ?>
-                        <p>Could not get information about the costs.</p>
-                        <?php
-                    }
-                    ?>
-                </section>
-                <section class="restaurantContentInfoPictureTime">
-                    <img class="imgIcons" src="/img/icons/clockb.png" alt="clock">
-                </section>
-                <section class="restaurantContentInfoSessions">
-                    <h3 class="informationRestaurantLabel">Sessions</h3>
-                    <?php
-
-                    if ($times == null || count($times) == 0) {
-                        echo "<p>There are no times at this restaurant.</p>";
-                    } else {
-                        foreach ($times as $beginTime => $endTime) {
-                            echo "<p class='infotext'>$beginTime-$endTime</p>";
-                        }
-                    }
-                    ?>
-                </section>
-                <section class="restaurantContentInfoDays">
-                    <h3 class="informationRestaurantLabel">Days</h3>
-                    <?php
-                    if ($dates == null || count($dates) == 0) {
-                        echo "<p>There are no dates at this restaurant.</p>";
-                    } else {
-                        foreach ($dates as $date) {
-                            echo "<p>$date</p>";
-                        }
-                    }
-                    ?>
-                </section>
-                <section class="restaurantContentInfoPictureLocation">
-                    <p><img class="imgIcons" src="/img/icons/location.png" alt="location"></p>
-                </section>
-                <section class="restaurantContentInfoAddress">
-                    <h3 class="informationRestaurantLabel">Location</h3>
-                    <?php
-                    if (isset($location)) {
-                        ?>
-                        <p><?php echo $location ?> Haarlem</p>
-                        <?php
-                    } else {
-                        ?>
-                        <p>No location information about this restaurant.</p>
-                        <?php
-                    }
-                    ?>
-                </section>
-                <section class="restaurantContentInfoPictureLinks">
-                    <img class="imgIcons" id="restaurantContentInfoPictureLinks" src="/img/icons/links.png" alt="links">
-                </section>
-                <section class="restaurantContentInfoLinks">
-                    <?php
-                    if (!empty($website)) {
-                        ?>
-                        <h3 class="informationRestaurantLabel">Location</h3>
-                        <a href="<?php echo $website ?>">Website</a><br>
-                        <a href="<?php echo $menu ?>">Menu</a><br>
-                        <a href="<?php echo $contactpage ?>">Contact</a>
-                    <?php
-                    }
-                    else{
-                    ?>
-                        <script>
-                            var link = document.getElementById('restaurantContentInfoPictureLinks');
-                            link.style.display = 'none'; //or
-                            link.style.visibility = 'hidden';
-                        </script>
-                        <?php
-                    }
-                    ?>
-                </section>
-            </section>
-            <?php
-        }
-
-        ?>
+//        function echoRestaurant($restaurant)
+//        {
+//            $restaurantName = $restaurant->getName();
+//            $description = $restaurant->getDescription();
+//            $stars = $restaurant->getStars();
+//            $restaurantId = $restaurant->getId();
+//            $location = $restaurant->getLocation()->getAddress() . ", " . $restaurant->getLocation()->getPostalCode();
+//            $parking = $restaurant->getParking();
+//            $price = $restaurant->getPrice();
+//            $childrenPrice = $restaurant->getPrice() / 2;
+//
+//            $seats = $restaurant->getSeats();
+//
+//            $restaurantService = new restaurantService();
+//            $times = $restaurantService->getTimesByRestaurantId($restaurantId);
+//            $dates = $restaurantService->getDatesByRestaurantId($restaurantId);
+//
+//            // Contact
+//            $phoneNumber = $restaurant->getPhoneNumber();
+//            $website = $restaurant->getWebsite();
+//            $contactpage = $restaurant->getContact();
+//            $menu = $restaurant->getMenu();
+//            ?>
+<!---->
+<!--            <section class="container">-->
+<!--                <section class="restaurantContentDescription">-->
+<!--                    <h2 id='starsHeader'>--><?php //echo $restaurantName ?><!--</h2>-->
+<!--                    --><?php
+//                    for ($x = 0; $x < $stars; $x++) {
+//                        echo "<img class='stars' src='/img/icons/star.png' alt='ster'>";
+//                    }
+//
+//                    if (!empty($description)) {
+//                        ?>
+<!---->
+<!--                        <p> --><?php //echo $description ?><!--</p>-->
+<!---->
+<!--                        --><?php
+//                    }
+//
+//                    if (!empty($parking)) {
+//                        ?>
+<!--                        <br><h3>Parking information</h3>-->
+<!--                        <p>--><?php //echo $parking ?><!--</p>-->
+<!--                        --><?php
+//                    }
+//                    ?>
+<!--                    <br>-->
+<!--                    <p><img src="/img/Restaurants/restaurant--><?php //echo $restaurantId ?><!--.png"-->
+<!--                            alt="Photo of --><?php //echo $restaurantName ?><!--" width="300"></p>-->
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoPictureCosts">-->
+<!--                    <img class="imgIcons" src="/img/icons/costs.png" alt="costs">-->
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoCosts">-->
+<!--                    <h3 class="informationRestaurantLabel">Costs</h3>-->
+<!--                    --><?php
+//                    if (!empty($price) && !empty($childrenPrice)) {
+//                        ?>
+<!--                        <p><i>Diner costs</i></p>-->
+<!--                        <p>€--><?php //echo $price ?><!-- p.p.</p>-->
+<!--                        <sup>Children til 12 years: € --><?php //echo $childrenPrice ?><!-- p.p.</sup>-->
+<!---->
+<!--                        <p><i>Reservation costs</i></p>-->
+<!--                        <p>€10 </p>-->
+<!--                        --><?php
+//                    } else {
+//                        ?>
+<!--                        <p>Could not get information about the costs.</p>-->
+<!--                        --><?php
+//                    }
+//                    ?>
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoPictureTime">-->
+<!--                    <img class="imgIcons" src="/img/icons/clockb.png" alt="clock">-->
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoSessions">-->
+<!--                    <h3 class="informationRestaurantLabel">Sessions</h3>-->
+<!--                    --><?php
+//
+//                    if ($times == null || count($times) == 0) {
+//                        echo "<p>There are no times at this restaurant.</p>";
+//                    } else {
+//                        foreach ($times as $beginTime => $endTime) {
+//                            echo "<p class='infotext'>$beginTime-$endTime</p>";
+//                        }
+//                    }
+//                    ?>
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoDays">-->
+<!--                    <h3 class="informationRestaurantLabel">Days</h3>-->
+<!--                    --><?php
+//                    if ($dates == null || count($dates) == 0) {
+//                        echo "<p>There are no dates at this restaurant.</p>";
+//                    } else {
+//                        foreach ($dates as $date) {
+//                            echo "<p>$date</p>";
+//                        }
+//                    }
+//                    ?>
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoPictureLocation">-->
+<!--                    <p><img class="imgIcons" src="/img/icons/location.png" alt="location"></p>-->
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoAddress">-->
+<!--                    <h3 class="informationRestaurantLabel">Location</h3>-->
+<!--                    --><?php
+//                    if (isset($location)) {
+//                        ?>
+<!--                        <p>--><?php //echo $location ?><!-- Haarlem</p>-->
+<!--                        --><?php
+//                    } else {
+//                        ?>
+<!--                        <p>No location information about this restaurant.</p>-->
+<!--                        --><?php
+//                    }
+//                    ?>
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoPictureLinks">-->
+<!--                    <img class="imgIcons" id="restaurantContentInfoPictureLinks" src="/img/icons/links.png" alt="links">-->
+<!--                </section>-->
+<!--                <section class="restaurantContentInfoLinks">-->
+<!--                    --><?php
+//                    if (!empty($website)) {
+//                        ?>
+<!--                        <h3 class="informationRestaurantLabel">Location</h3>-->
+<!--                        <a href="--><?php //echo $website ?><!--">Website</a><br>-->
+<!--                        <a href="--><?php //echo $menu ?><!--">Menu</a><br>-->
+<!--                        <a href="--><?php //echo $contactpage ?><!--">Contact</a>-->
+<!--                    --><?php
+//                    }
+//                    else{
+//                    ?>
+<!--                        <script>-->
+<!--                            var link = document.getElementById('restaurantContentInfoPictureLinks');-->
+<!--                            link.style.display = 'none'; //or-->
+<!--                            link.style.visibility = 'hidden';-->
+<!--                        </script>-->
+<!--                        --><?php
+//                    }
+//                    ?>
+<!--                </section>-->
+<!--            </section>-->
+<!--            --><?php
+//        }
+//
+//        ?>
     </section>
 </main>
 </body>
