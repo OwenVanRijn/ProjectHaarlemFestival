f95f8d1e6d6caa453605176a550953387a6c4768
Lou <louellacreemers@gmail.com>
2021-03-25 12:40:40 +0100

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -2,28 +2,29 @@
 require_once "./lib/dompdf/autoload.inc.php";
 
 ob_start();
-include 'htmlpdf.php';
+include 'ticketpdf.php';
 $rawHtml = ob_get_clean();
 
+var_dump($rawHtml);
 
-use Dompdf\Dompdf;
-use Dompdf\Options;
-
-$options = new Options();
-$options->set('defaultFont', 'Courier');
-
-
-$dompdf = new Dompdf($options);
-$dompdf->loadHtml($rawHtml);
-
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
-
-// Render the HTML as PDF
-$dompdf->render();
-
-// Output the generated PDF to Browser
-$dompdf->stream();
+//use Dompdf\Dompdf;
+//use Dompdf\Options;
+//
+//$options = new Options();
+//$options->set('defaultFont', 'Courier');
+//
+//
+//$dompdf = new Dompdf($options);
+//$dompdf->loadHtml($rawHtml);
+//
+//// (Optional) Setup the paper size and orientation
+//$dompdf->setPaper('A4', 'landscape');
+//
+//// Render the HTML as PDF
+//$dompdf->render();
+//
+//// Output the generated PDF to Browser
+//$dompdf->stream();
 
 ?>
 
