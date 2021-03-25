560adf6fe6fff88a46464968e60474d7719c0b6a
Lou <louellacreemers@gmail.com>
2021-03-25 14:20:59 +0100

    Update dance.php

--- dance.php
+++ dance.php
@@ -52,6 +52,7 @@ if (isset($_POST['addticket'])) {
     <title>Dance - Haarlem Festival</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/navBarFooter.css">
+    <link rel="stylesheet" href="css/dance.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="keywords"
