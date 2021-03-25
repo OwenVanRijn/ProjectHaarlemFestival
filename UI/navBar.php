55003b6b98097e21f089fe00eaadf66e3ed34776
Lou <louellacreemers@gmail.com>
2021-03-25 11:54:20 +0100

    Update navBar.php

--- UI/navBar.php
+++ UI/navBar.php
@@ -1,26 +1,26 @@
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!---->
-<!--<head>-->
-<!--        <link rel="stylesheet" href="css/navBarFooter.css">-->
-<!--        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
-<!--</head>-->
-<!---->
-<!--<body>-->
-<!--    <nav>-->
-<!--        <ul>-->
-<!--          <li class="navItem"><a class="image"><img class="logo" src="img/logo-wit.png" alt="logo"></a></li>-->
-<!--          <li><a class="navItem" href="./index.php">Home</a></li>-->
-<!--          <li><a class="navItem" class="navItem" href="./jazz.php">Jazz</a></li>-->
-<!--          <li><a class="navItem" href="./food.php">Food</a></li>-->
-<!--          <li><a class="navItem" href="./dance.php">Dance</a></li>-->
-<!--          <li><a class="navItem" href="./contact.php">Contact</a></li>-->
-<!--          <li><a class="imageRight"><img class="logo" src="img/translate.png" alt="logo"></a></li>-->
-<!--          <li><a class="imageRight" href="./shoppingcart.php"><img class="logo" src="img/account.png" alt="account"></a></li>-->
-<!--          <li><a class="imageRight"><img class="logo" src="img/shoppingcart.png" alt="shoppingcart"></a></li>-->
-<!--        </ul>-->
-<!--     </nav>-->
-<!---->
-<!---->
-<!--</body>-->
-<!--</html>-->
+<!DOCTYPE html>
+<html>
+
+<head>
+        <link rel="stylesheet" href="css/navBarFooter.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <nav>
+        <ul>
+          <li class="navItem"><a class="image"><img class="logo" src="img/logo-wit.png" alt="logo"></a></li>
+          <li><a class="navItem" href="./index.php">Home</a></li>
+          <li><a class="navItem" class="navItem" href="./jazz.php">Jazz</a></li>
+          <li><a class="navItem" href="./food.php">Food</a></li>
+          <li><a class="navItem" href="./dance.php">Dance</a></li>
+          <li><a class="navItem" href="./contact.php">Contact</a></li>
+          <li><a class="imageRight"><img class="logo" src="img/translate.png" alt="logo"></a></li>
+          <li><a class="imageRight" href="./shoppingcart.php"><img class="logo" src="img/account.png" alt="account"></a></li>
+          <li><a class="imageRight"><img class="logo" src="img/shoppingcart.png" alt="shoppingcart"></a></li>
+        </ul>
+     </nav>
+
+
+</body>
+</html>
