09ce351e780bae8ffdd84e31b3c7534c2271a3fe
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-03-25 14:30:42 +0100

    Update dance.php

--- dance.php
+++ dance.php
@@ -46,181 +46,171 @@ if (isset($_POST['addticket'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
-
+<html lang="en" dir="ltr">
 <head>
-    <title>Dance - Haarlem Festival</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navBarFooter.css">
-    <link rel="stylesheet" href="css/dance.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="keywords"
           content="Haarlem, festival, jazz, food, history, party, feest, geschiedenis, eten, restaurant">
     <meta name="description" content="Haarlem Festival">
     <meta name="author" content="Haarlem Festival">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" ;
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-            integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
-            crossorigin="anonymous"></script>
+    <title>Dance</title>
 </head>
-
 <body>
 <?php
 genNavBar();
 ?>
-<header>
-    <div class="title">
-        <h1 class="main-title">Dance</h1>
-        <p class="main-title under-title">Haarlem Festival, for the people that enjoy
-            <br>a good time
-        </p>
-    </div>
+<header class="title">
+
+    <h1 class="main-title">Dance</h1>
+    <p class="main-title under-title">Haarlem Festival, for the people that enjoy a good time</p>
+
 </header>
+<main class="content">
+    <section class="container h-100">
 
-<section class="container h-100">
+        <?php
+        $service = new artistService();
+        (array)$artistArray = $service->getArtists();
 
-    <?php
-    $service = new artistService();
-    (array)$artistArray = $service->getArtists();
-
-    //all-access
-    echo "<section class='row h-100 align-items-center' style='background-color: #C0C0C0; margin: 2% 25% 2% 25%;
+        //all-access
+        echo "<section class='row h-100 align-items-center' style='background-color: #C0C0C0; margin: 2% 25% 2% 25%;
                     padding: 1% 0 1% 0;'>";
-    echo "<section class='col-8 fonttickets'>All in ticket (Friday/Saturday/Sunday): €250,-</section>";
-    echo "<section class='col-4 text-right'><button class='btn btn-primary'>Add Ticket</button></section>";
-    echo "</section>";
+        echo "<section class='col-8 fonttickets'>All in ticket (Friday/Saturday/Sunday): €250,-</section>";
+        echo "<section class='col-4 text-right'><button class='btn btn-primary'>Add Ticket</button></section>";
+        echo "</section>";
 
-    //Lineup
-    echo "<section class= 'row' style='padding: 1em'>";
-    foreach ($artistArray as $item) {
-        $name = $item->getName();
-        $nameStripped = strtolower(str_replace(' ', '', $name));
+        //Lineup
+        echo "<section class= 'row' style='padding: 1em'>";
+        foreach ($artistArray as $item) {
+            $name = $item->getName();
+            $nameStripped = strtolower(str_replace(' ', '', $name));
 
-        echo "<section class='col-2'>
+            echo "<section class='col-2'>
                 <img src='img/Artists/{$nameStripped}.png' class='w-100' alt='{$name}'>
                 <a href = 'danceartist.php?name={$name}' class='btn btn-primary' style='border-radius: 0;'>Click here for {$name} performances</a></section>";
-    }
-    echo "</section>";
-    ?>
-</section>
-
-<section class="container-fluid">
-
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
-        <section class="row h-100 align-items-center" style="background-color: #C0C0C0">
-            <section class="col-3 text-center fonttickets"
-                     style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php?day=friday"
-                                                                                         style="color: black;">Friday
-                    27th of July</a></section>
-            <section class="col-3 text-center fonttickets"
-                     style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php?day=saturday"
-                                                                                         style="color: black;">Saturday
-                    28th of July</a></section>
-            <section class="col-3 text-center fonttickets"
-                     style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php?day=sunday"
-                                                                                         style="color: black;">Sunday
-                    29th of July</a></section>
-            <section class="col-3 text-center fonttickets"
-                     style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php"
-                                                                                         style="color: black;">All
-                    days</a></section>
-        </section>
-    </form>
-
-    <section class="row h-100 align-items-center">
-        <section class="col text-center fonttickets" style="padding: 0.5em; background-color: #FD6A02;">
-            <a class="btn btn-primary w-100" data-toggle="collapse" href="#filtercollapse">Filters ˅</a>
-        </section>
+        }
+        echo "</section>";
+        ?>
     </section>
 
-    <section class="collapse in w-100" id="filtercollapse">
-        <section class="card card-body w-100" style="background-color: #FD6A02;">
-            <section class="row">
-                <section class="col-4">
-                    <p>Artists:</p>
-                    <?php
-                    $artistService = new artistService();
-                    (array)$artistArray = $artistService->getArtists();
+    <section class="container-fluid">
 
-                    //TODO Duplicate code removal
-                    foreach ($artistArray as $item) {
-                        $name = $item->getName();
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
+            <section class="row h-100 align-items-center" style="background-color: #C0C0C0">
+                <section class="col-3 text-center fonttickets"
+                         style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php?day=friday"
+                                                                                             style="color: black;">Friday
+                        27th of July</a></section>
+                <section class="col-3 text-center fonttickets"
+                         style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a
+                            href="dance.php?day=saturday"
+                            style="color: black;">Saturday
+                        28th of July</a></section>
+                <section class="col-3 text-center fonttickets"
+                         style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php?day=sunday"
+                                                                                             style="color: black;">Sunday
+                        29th of July</a></section>
+                <section class="col-3 text-center fonttickets"
+                         style="border-width:0.2em; border-style: solid; padding: 0.5em;"><a href="dance.php"
+                                                                                             style="color: black;">All
+                        days</a></section>
+            </section>
+        </form>
 
-                        echo "<section class='row'>";
-                        echo "<input type='radio' name='artistSelected' id= '{$name}' value='{$name}'>";
-                        echo "<label for='{$name}'>$name</label>";
-                        echo "</section>";
-                    }
-                    ?>
-                </section>
+        <section class="row h-100 align-items-center">
+            <section class="col text-center fonttickets" style="padding: 0.5em; background-color: #FD6A02;">
+                <a class="btn btn-primary w-100" data-toggle="collapse" href="#filtercollapse">Filters ˅</a>
+            </section>
+        </section>
 
-                <section class="col-4">
-                    <!--                        <p>Type:</p>-->
-                    <!--                        --><?php
-                    //                            $activityService = new danceActivityService();
-                    //                            $activityArray = $activityService->
-                    //                        ?>
-                </section>
-                <section class="col-4">
+        <section class="collapse in w-100" id="filtercollapse">
+            <section class="card card-body w-100" style="background-color: #FD6A02;">
+                <section class="row">
+                    <section class="col-4">
+                        <p>Artists:</p>
+                        <?php
+                        $artistService = new artistService();
+                        (array)$artistArray = $artistService->getArtists();
 
+                        //TODO Duplicate code removal
+                        foreach ($artistArray as $item) {
+                            $name = $item->getName();
+
+                            echo "<section class='row'>";
+                            echo "<input type='radio' name='artistSelected' id= '{$name}' value='{$name}'>";
+                            echo "<label for='{$name}'>$name</label>";
+                            echo "</section>";
+                        }
+                        ?>
+                    </section>
+
+                    <section class="col-4">
+                        <!--                        <p>Type:</p>-->
+                        <!--                        --><?php
+                        //                            $activityService = new danceActivityService();
+                        //                            $activityArray = $activityService->
+                        //                        ?>
+                    </section>
+                    <section class="col-4">
+
+                    </section>
                 </section>
             </section>
         </section>
-    </section>
 
 
-    <section class="container-fluid">
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <?php
-            echo "<section class='row' style='margin-top: 2%'>";
+        <section class="container-fluid">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <?php
+                echo "<section class='row' style='margin-top: 2%'>";
 
-            foreach ($activeArray as $item) {
+                foreach ($activeArray as $item) {
 
-                echo "<section class='col-4 box'>";
-                echo "<section class='col-12 text-center' style='background-color: black; color: white; padding-top: 2%;'>";
-                $artiststrarray = $item->getArtists();
-                $artists = "";
+                    echo "<section class='col-4 box'>";
+                    echo "<section class='col-12 text-center' style='background-color: black; color: white; padding-top: 2%;'>";
+                    $artiststrarray = $item->getArtists();
+                    $artists = "";
 
-                foreach ($artiststrarray as $artist) {
-                    $artists .= $artist->getName() . " ";
+                    foreach ($artiststrarray as $artist) {
+                        $artists .= $artist->getName() . " ";
+                    }
+
+                    $id = $item->getId();
+                    $time = $item->getActivity()->getStartTime()->format("H:i");
+
+                    $location = $item->getActivity()->getLocation()->getName();
+
+                    $session = $item->getType();
+
+                    $price = "€" . $item->getActivity()->getPrice() . ",-";
+
+                    echo "<p style='color: orange; font-weight: bold'>{$artists}</p>";
+                    echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Start time:</p>{$time}</section>";
+                    echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Location:</p><bold>{$location}</bold></section>";
+                    echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Session:</p><bold>{$session}</bold></section>";
+                    echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Price:</p><bold>{$price}</bold></section>";
+                    echo "</section>";
+
+
+                    echo "<input name='id' type='hidden' value='{$id}'>";
+                    echo "<button class= 'btn btn-primary w-100' type='submit' name='addticket' value='{$id}'>Add to cart</button>";
+                    echo "</section>";
+
                 }
-
-                $id = $item->getId();
-                $time = $item->getActivity()->getStartTime()->format("H:i");
-
-                $location = $item->getActivity()->getLocation()->getName();
-
-                $session = $item->getType();
-
-                $price = "€" . $item->getActivity()->getPrice() . ",-";
-
-                echo "<p style='color: orange; font-weight: bold'>{$artists}</p>";
-                echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Start time:</p>{$time}</section>";
-                echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Location:</p><bold>{$location}</bold></section>";
-                echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Session:</p><bold>{$session}</bold></section>";
-                echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Price:</p><bold>{$price}</bold></section>";
                 echo "</section>";
-
-
-                echo "<input name='id' type='hidden' value='{$id}'>";
-                echo "<button class= 'btn btn-primary w-100' type='submit' name='addticket' value='{$id}'>Add to cart</button>";
-                echo "</section>";
-
-            }
-            echo "</section>";
-            ?>
-        </form>
+                ?>
+            </form>
+        </section>
     </section>
-</section>
-
+</main>
+<?php
+require_once "UI/footer.php";
+?>
 </body>
-
 </html>
