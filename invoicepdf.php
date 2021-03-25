1d5d34c8efac0bbfd8745ce350c7f771a64b751f
Lou <louellacreemers@gmail.com>
2021-03-25 12:21:26 +0100

    pdf update

--- invoicepdf.php
+++ invoicepdf.php
@@ -1,7 +1,12 @@
 <?php
+ini_set('display_errors', -1);
+require_once  "./Service/ticketService.php";
+require_once  "./Service/activityService.php";
+require_once "./lib/barcodegen/vendor/autoload.php";
+
 $activity = new activityService();
 $id = 36; //$_SESSION['orderId'];
-$total = 35.00;$_SESSION['total'];
+$total = 35.00;//$_SESSION['total'];
 
 $ticket = new ticketService();
 
@@ -12,9 +17,9 @@ $returnTick = $ticket->getTicketsByOrder($id);
 <html>
 <body>
 <?php
-echo $returnTick->getActivity()->getPrice();
+echo "price: ".$returnTick->getActivity()->getPrice();
 echo "<br>";
-echo $total;
+echo "total:" . $total;
 ?>
 </body>
 </html>
---
+++ ticketpdf.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+
+ini_set('display_errors', -1);
+require_once  "./Service/ticketService.php";
+require_once  "./Service/activityService.php";
+require_once "./lib/barcodegen/vendor/autoload.php";
+
+$activity = new activityService();
+$id = $_SESSION['orderId'];
+$ticket = new ticketService();
+
+$returnTick = $ticket->getTicketsByOrder($id);
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css";
+    </head>
+
+    <body>
+        <section class="text-center">
+            <h1>HAARLEM FESTIVAL TICKET(S)</h1>
+        </section>
+
+        <section class="container">
+            <?php
+
+            if(is_object($returnTick)){
+                echo "<section class = 'row' style='border-style: solid; margin: 2%; padding: 2%'>";
+                echo "<section class='col-sm-10'>";
+                $date = date_format($returnTick->getActivity()->getDate(), "d/m/y");
+                $startTime = date_format($returnTick->getActivity()->getStartTime(), "H:i");
+                $endTime = date_format($returnTick->getActivity()->getEndTime(), "H:i");
+                $type = $returnTick->getActivity()->getType();
+                $price = $returnTick->getActivity()->getPrice();
+                $location = $returnTick->getActivity()->getLocation()->getName();
+
+                echo "<p>{$type} - {$startTime} / {$endTime} @ {$date}. Location: {$location}, Price: {$price}EUR</p>";
+                echo "</section>";
+
+                echo "<section class='col-sm-2'>";
+                $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+                echo $generator->getBarcode($returnTick->getId(), $generator::TYPE_CODE_128);
+                echo "</section>";
+                echo "</section>";
+            }
+
+            else{
+                foreach ($returnTick as $item){
+                    echo "<section class = 'row' style='border-style: solid; margin: 2%; padding: 2%'>";
+                    echo "<section class='col-sm-10'>";
+                    $date = date_format($item->getActivity()->getDate(), "d/m/y");
+                    $startTime = date_format($item->getActivity()->getStartTime(), "H:i");
+                    $endTime = date_format($item->getActivity()->getEndTime(), "H:i");
+                    $type = $item->getActivity()->getType();
+                    $price = $item->getActivity()->getPrice();
+                    $location = $item->getActivity()->getLocation()->getName();
+
+                    //$activityDetails = json_decode(json_encode($activity->getTypedActivityByIds([$item->getActivity()->getId()])), FALSE);
+                    //print_r($activityDetails->getLocation());
+
+                    echo "<p>{$type} - {$startTime} / {$endTime} @ {$date}. Location: {$location}, Price: {$price}EUR</p>";
+                    echo "</section>";
+
+                    echo "<section class='col-sm-2'>";
+                    $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+                    echo $generator->getBarcode($item->getId(), $generator::TYPE_CODE_128);
+                    echo "</section>";
+                    echo "</section>";
+                }
+            }
+            ?>
+        </section>
+    </body>
+</html>
