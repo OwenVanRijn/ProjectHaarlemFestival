74171c3bc5c915624698e26da1ad4e7b13dcdb85
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-03-29 13:15:39 +0200

    footer en nav

--- UI/footer.php
+++ UI/footer.php
@@ -8,32 +8,34 @@ $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 <html>
 
 <head>
-        <link rel="stylesheet" href="css/navBarFooter.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/navBarFooter.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-  <section class="share">
-      <p class="textShare">Share this page</p>
-      <img class="shareButtons" src="img/whatsapp.png" alt="">
-      <img class="shareButtons" src="img/email.png" alt="">
-  </section>
-    <footer>
-      <section class="footer-left">
-        <img class="logoFooter" src="img/logo-wit.png" alt="">
-      </section>
-      <section class="footer-right">
-        <p class="labelSocialMedia">Social media</p>
-        <a href="#" class="fa fa-facebook"></a>
-        <a href="#" class="fa fa-youtube"></a>
-      </section>
-      <section class="footer-center">
-        <ul>
-          <li class="footerLinks"><a href="./home.php">Home</a></li>
-          <li class="footerLinks"><a href="./socials.php">Social</a></li>
-          <li class="footerLinks"><a href="./contact.php">Contact us</a></li>
-          <li class="footerLinks"><a href="./shoppingcart.php">Shopping cart</a></li>
-          <li class="footerLinks"><a href="./volunteers.php">For volunteers</a></li>
-        </ul>
-      </section>
+<section class="share">
+    <p class="textShare">Share this page</p>
+    <img class="shareButtons" src="img/whatsapp.png" alt="">
+    <img class="shareButtons" src="img/email.png" alt="">
+</section>
+<footer>
+    <section class="footerFull">
+        <section class="footer-left">
+            <img class="logoFooter" src="img/logo-wit.png" alt="logo">
+        </section>
+        <section class="footer-right">
+            <p class="labelSocialMedia">Social media</p>
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-youtube"></a>
+        </section>
+        <section class="footer-center">
+            <ul>
+                <li class="footerLinks"><a href="./home.php">Home</a></li>
+                <li class="footerLinks"><a href="./socials.php">Social</a></li>
+                <li class="footerLinks"><a href="./contact.php">Contact us</a></li>
+                <li class="footerLinks"><a href="./shoppingcart.php">Shopping cart</a></li>
+                <li class="footerLinks"><a href="./volunteers.php">For volunteers</a></li>
+            </ul>
+        </section>
+    </section>
 </html>
--- UI/navBar.php
+++ UI/navBar.php
@@ -1,3 +1,10 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once($root . "/Service/shoppingcartService.php");
+$shoppingcartService = new shoppingcartService();
+$shoppingcartCount = $shoppingcartService->getShoppingcartItemsCount();
+
+?>
 <nav>
     <link rel="stylesheet" href="/css/navBarFooter.css">
     <ul>
@@ -7,8 +14,12 @@
         <li><a class="navItem" href="/food.php">Food</a></li>
         <li><a class="navItem" href="/dance.php">Dance</a></li>
         <li><a class="navItem" href="/contact.php">Contact</a></li>
-        <li><a class="imageRight"><img class="logo" src="/img/translate.png" alt="logo"></a></li>
-        <li><a class="imageRight"><img class="logo" src="/img/account.png" alt="account"></a></li>
-        <li><a class="imageRight" href="/shoppingcart.php"><img class="logo" src="/img/shoppingcart.png" alt="shoppingcart"></a></li>
+        <li><a class="imageRight" target="_blank" href="https://translate.google.com/translate?sl=auto&tl=en&u=http://haarlemfestival.louellacreemers.nl<?php echo $_SERVER['PHP_SELF']; ?>"><img class="logo" src="/img/translate.png" alt="logo"></a></li>
+        <li><a class="imageRight" href="/account.php"><img class="logo" src="/img/account.png" alt="account"></a></li>
+        <li><a href="/shoppingcart.php">
+            <section class="imageRight fa-stack fa-2x has-badge" data-count="<?php echo $shoppingcartCount ?>">
+                <img class="logo" src="/img/shoppingcart.png"
+                        alt="logo"></a>
+        </li>
     </ul>
 </nav>
