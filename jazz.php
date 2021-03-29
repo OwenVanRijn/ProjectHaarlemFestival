811fee24990e10df543905304595c7517a384439
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-03-29 12:14:26 +0200

    Update jazz.php

--- jazz.php
+++ jazz.php
@@ -1,7 +1,6 @@
 <?php
-$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-require_once($root . "/Service/jazzActivityService.php");
-require_once($root . "/Service/jazzbandService.php");
+require_once("/Service/jazzActivityService.php");
+require_once("/Service/jazzbandService.php");
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -19,7 +18,7 @@ require_once($root . "/Service/jazzbandService.php");
 </head>
 <body>
 <?php
-require_once($root . "/UI/navBar.php");
+require_once("/UI/navBar.php");
 ?>
 <header class="title">
 
