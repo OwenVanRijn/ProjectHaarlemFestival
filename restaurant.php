330d62f94921ee0a0b761746686db5e61de79a60
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-03-31 16:03:13 +0200

    Update restaurant.php

--- restaurant.php
+++ restaurant.php
@@ -1,5 +1,6 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 if (isset($_GET["restaurantId"])) {
     $restaurantId = $_GET["restaurantId"];
     var_dump($restaurantId);
