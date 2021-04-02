c88b8f9a5a011e6b6781cc7a6a42cf30779bc24f
Lou <louellacreemers@gmail.com>
2021-04-02 02:22:42 +0200

    Mollie push

--- pdf/ticketpdf.php
+++ pdf/ticketpdf.php
@@ -6,8 +6,8 @@ require_once $root . "/lib/barcodegen/vendor/autoload.php";
 
 $activity = new activityService();
 $ticket = new ticketService();
-$id = $_SESSION['orderId'];
-$returnTick = $ticket->getTicketsByOrder($id);
+$id = 76;//$_SESSION['orderId'];
+$ticketArray = $ticket->getTicketsByOrder($id);
 ?>
 
 <!DOCTYPE html>
@@ -24,29 +24,7 @@ $returnTick = $ticket->getTicketsByOrder($id);
 
         <section class="container">
             <?php
-
-            if(is_object($returnTick)){
-                echo "<section class = 'row' style='border-style: solid; margin: 2%; padding: 2%'>";
-                echo "<section class='col-sm-10'>";
-                $date = date_format($returnTick->getActivity()->getDate(), "d/m/y");
-                $startTime = date_format($returnTick->getActivity()->getStartTime(), "H:i");
-                $endTime = date_format($returnTick->getActivity()->getEndTime(), "H:i");
-                $type = $returnTick->getActivity()->getType();
-                $price = $returnTick->getActivity()->getPrice();
-                $location = $returnTick->getActivity()->getLocation()->getName();
-
-                echo "<p>{$type} - {$startTime} / {$endTime} @ {$date}. Location: {$location}, Price: {$price}EUR</p>";
-                echo "</section>";
-
-                echo "<section class='col-sm-2'>";
-                $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-                echo $generator->getBarcode($returnTick->getId(), $generator::TYPE_CODE_128);
-                echo "</section>";
-                echo "</section>";
-            }
-
-            else{
-                foreach ($returnTick as $item){
+                foreach ($ticketArray as $item){
                     echo "<section class = 'row' style='border-style: solid; margin: 2%; padding: 2%'>";
                     echo "<section class='col-sm-10'>";
                     $date = date_format($item->getActivity()->getDate(), "d/m/y");
@@ -55,9 +33,6 @@ $returnTick = $ticket->getTicketsByOrder($id);
                     $type = $item->getActivity()->getType();
                     $price = $item->getActivity()->getPrice();
                     $location = $item->getActivity()->getLocation()->getName();
-
-                    //$activityDetails = json_decode(json_encode($activity->getTypedActivityByIds([$item->getActivity()->getId()])), FALSE);
-                    //print_r($activityDetails->getLocation());
 
                     echo "<p>{$type} - {$startTime} / {$endTime} @ {$date}. Location: {$location}, Price: {$price}EUR</p>";
                     echo "</section>";
@@ -68,7 +43,6 @@ $returnTick = $ticket->getTicketsByOrder($id);
                     echo "</section>";
                     echo "</section>";
                 }
-            }
             ?>
         </section>
     </body>
