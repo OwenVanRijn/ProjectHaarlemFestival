89d619d4c81cc1646d23bd38deea5404c0b53d08
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-04-03 17:46:18 +0200

    final touch

--- danceartist.php
+++ danceartist.php
@@ -79,7 +79,7 @@
                         echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Session:</p><bold>{$session}</bold></section>";
                         echo "<section class='row justify-content-center align-self-center text-center'><p style='color: orange; font-weight: bold'>Price:</p><bold>{$price}</bold></section>";
                         echo "</section>";
-                        echo "<button class='button1 w-100' type='submit' name='select' value='{$id}'>Add to cart</a>";
+                        echo "<button class='btn btn-primary w-100' type='submit' name='select' value='{$id}'>Add to cart</a>";
                         echo "</section>";
                     }
                     ?>
--- volunteers.php
+++ volunteers.php
@@ -11,7 +11,7 @@
 <head>
     <title>FOR VOLUNTEERS - Haarlem Festival</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css";
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <meta charset="UTF-8">
     <meta name="keywords" content="Haarlem, festival, jazz, food, history, party, feest, geschiedenis, eten, restaurant">
     <meta name="description" content="Haarlem Festival">
