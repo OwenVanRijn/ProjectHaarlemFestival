4341c251a8c061f4c4b79b43ee432200096910c5
SanderBrijer <54888531+SanderBrijer@users.noreply.github.com>
2021-04-06 11:34:37 +0200

    Update restaurant.php

--- Model/restaurant.php
+++ Model/restaurant.php
@@ -170,7 +170,7 @@ class restaurant extends sqlModel
      */
     public function getMenu(): string
     {
-        return $this->website;
+        return $this->menu;
     }
 
     public function getFullMenu()
