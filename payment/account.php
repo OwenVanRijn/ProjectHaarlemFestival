ae2f50a6de217c1de88fd87bb29136f19282a38a
Lou <louellacreemers@gmail.com>
2021-04-07 20:42:55 +0200

    account update

--- payment/account.php
+++ payment/account.php
@@ -27,73 +27,7 @@ if (isset($_POST['submit'])) {
 ?>
 
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Payment - account</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/shoppingcart.css">
-    <link rel="stylesheet" href="../css/navBarFooter.css">
-    <meta charset="UTF-8">
-    <meta name="keywords"
-          content="Haarlem, festival, jazz, food, history, party, feest, geschiedenis, eten, restaurant">
-    <meta name="description" content="Haarlem Festival">
-    <meta name="author" content="Haarlem Festival">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-
-<section class="contentShoppingcart content">
-    <section class="contentAccount">
-        <h1>Payment</h1>
-
-        <section>
-            <p class="step">Step 1 / 3</p>
-        </section>
-
-
-        <form method="post">
-            <h2>Your information</h2>
-            <h4>Please fill in your personal information</h4>
-
-
-            <h4 class="labelInputField">Firstname</h4>
-            <input type="text" placeholder="firstname" name="firstname" maxlength="40" size="20" required></input>
-
-            <h4 class="labelInputField">Lastname</h4>
-            <input type="text" placeholder="lastname" maxlength="40" name="lastname" size="20" required></input>
-
-            <h4 class="labelInputField">Emailaddress</h4>
-            <input type="text" placeholder="emailaddress" maxlength="40" name="email" size="25" required></input>
-
-
-            <br><input type="checkbox" id="account" name="account" value="account"
-                       onclick="displayAccountFields('moreAccountFields', this)">
-            <label for="account"> Make an account</label><br>
-
-
-            <section id="moreAccountFields" style="display:none">
-                <h4 class="labelInputField">Password</h4>
-                <input type="password" placeholder="password"></input>
-
-                <h4 class="labelInputField">Phonenumber</h4>
-                <input type="text" placeholder="phonenumber" maxlength="10" size="10"></input>
-            </section>
-
-            <script type="text/javascript">
-                function displayAccountFields(it, box) {
-                    var visable = (box.checked) ? "block" : "none";
-                    document.getElementById(it).style.display = visable;
-                }
-            </script>
-            <section>
-                <input class="stepNext" type="submit" name="submit" value="Next step">
-            </section>
-        </form>
-    </section>
-</section>
-</body>
-
-</html>
+<?php
+if(isset($_GET['t'])){
+   echo file_get_contents('accountHtml.php');
+}
